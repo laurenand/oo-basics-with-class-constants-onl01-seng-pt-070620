58a34ee89a5f3da2c90c9f6d6b27c44fb8d8d73e
Lauren Anderson <laurenaanderson95@gmail.com>
2020-07-23 01:12:05 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -5,7 +5,9 @@ class Shoe
   BRANDS=[]
   def initialize(brand)
     @brand = brand
-    BRANDS << brand
+    if brand is in BRANDS
+      BRANDS << brand
+    else 
   end
   def cobble
     self.condition = "new"
